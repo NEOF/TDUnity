1ebb1f14181eb219d5a02825ea24d4c948272a3f
NEOF <nocio@yandex.ru>
2013-10-03 16:25:18 -0600

    Archer Start
    Added 3 lines of triggers for archers, Attack script will work for
    both, warriors and archers. Now we also actually have top-down view, y
    coordinate is top.

--- TowerDefenceProject/Assets/Attack.cs
+++ TowerDefenceProject/Assets/Attack.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WarriorAttack : MonoBehaviour {
+public class Attack : MonoBehaviour {
 	// Use this for initialization
 	public float timeStamp;
 	
